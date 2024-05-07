9a1f96f75142a10d1e1f16dc8ebffa0e97e2c897
JuliaRDavid <juliaadavid@gmail.com>
2024-05-07 11:45:46 -0700

    fixed m_vx in Particle constructor

--- Particles/Particle.cpp
+++ Particles/Particle.cpp
@@ -4,23 +4,31 @@ Particle::Particle(RenderTarget& target, int numPoints, Vector2i mouseClickPosit
 {
     m_ttl = TTL;
     m_numPoints = numPoints;
-    m_radiansPerSec = (float)rand() / (RAND_MAX) * M_PI;
+    m_radiansPerSec = static_cast<float>(rand()) / (RAND_MAX) * M_PI;
 
     m_cartesianPlane.setCenter(0, 0);
     m_cartesianPlane.setSize(target.getSize().x, (-1.0) * target.getSize().y);
     m_centerCoordinate = target.mapPixelToCoords(mouseClickPosition, m_cartesianPlane);
 
-    m_vx = rand() % 1000 - 500;
+    int vx = rand() % 401 + 100;
+    if (vx % 2 == 0)
+    {
+        m_vx = vx * -1;
+    }
+    else 
+    {
+        m_vx = vx;
+    }
     m_vy = rand() % 401 + 100;
     m_color1 = Color{ 0, 0, 0 };
-    m_color2 = Color{ static_cast<Uint8>(rand() % 255), static_cast<Uint8>(rand() % 255), static_cast<Uint8>(rand() % 255) };
+    m_color2 = Color{ static_cast<Uint8>(rand() % 256), static_cast<Uint8>(rand() % 256), static_cast<Uint8>(rand() % 256) };
 
     float theta = static_cast<float>(rand()) / static_cast<float>( (RAND_MAX / (M_PI / 2)));
     float dTheta = 2 * M_PI / (numPoints - 1);
 
     for (int j = 0; j < numPoints; j++)
     {
-        float r = rand() % 60 + 20;
+        float r = rand() % 61 + 20;
         float dx = r * cos(theta);
         float dy = r * sin(theta);
         m_A(0, j) = m_centerCoordinate.x + dx;
@@ -61,15 +69,148 @@ void Particle::update(float dt)
 
 
 //Functions for unit testing
-bool Particle::almostEqual(double a, double b, double eps = 0.0001)
+bool Particle::almostEqual(double a, double b, double eps)
 {
-
-
+    return fabs(a - b) < eps;
 }
+
 void Particle::unitTests()
 {
-    
+    int score = 0;
 
+    cout << "Testing RotationMatrix constructor...";
+    double theta = M_PI / 4.0;
+    RotationMatrix r(M_PI / 4);
+    if (r.getRows() == 2 && r.getCols() == 2 && almostEqual(r(0, 0), cos(theta))
+        && almostEqual(r(0, 1), -sin(theta))
+        && almostEqual(r(1, 0), sin(theta))
+        && almostEqual(r(1, 1), cos(theta)))
+    {
+        cout << "Passed.  +1" << endl;
+        score++;
+    }
+    else
+    {
+        cout << "Failed." << endl;
+    }
+
+    cout << "Testing ScalingMatrix constructor...";
+    ScalingMatrix s(1.5);
+    if (s.getRows() == 2 && s.getCols() == 2
+        && almostEqual(s(0, 0), 1.5)
+        && almostEqual(s(0, 1), 0)
+        && almostEqual(s(1, 0), 0)
+        && almostEqual(s(1, 1), 1.5))
+    {
+        cout << "Passed.  +1" << endl;
+        score++;
+    }
+    else
+    {
+        cout << "Failed." << endl;
+    }
+
+    cout << "Testing TranslationMatrix constructor...";
+    TranslationMatrix t(5, -5, 3);
+    if (t.getRows() == 2 && t.getCols() == 3
+        && almostEqual(t(0, 0), 5)
+        && almostEqual(t(1, 0), -5)
+        && almostEqual(t(0, 1), 5)
+        && almostEqual(t(1, 1), -5)
+        && almostEqual(t(0, 2), 5)
+        && almostEqual(t(1, 2), -5))
+    {
+        cout << "Passed.  +1" << endl;
+        score++;
+    }
+    else
+    {
+        cout << "Failed." << endl;
+    }
+
+
+    cout << "Testing Particles..." << endl;
+    cout << "Testing Particle mapping to Cartesian origin..." << endl;
+    if (m_centerCoordinate.x != 0 || m_centerCoordinate.y != 0)
+    {
+        cout << "Failed.  Expected (0,0).  Received: (" << m_centerCoordinate.x << "," << m_centerCoordinate.y << ")" << endl;
+    }
+    else
+    {
+        cout << "Passed.  +1" << endl;
+        score++;
+    }
+
+    cout << "Applying one rotation of 90 degrees about the origin..." << endl;
+    Matrix initialCoords = m_A;
+    rotate(M_PI / 2.0);
+    bool rotationPassed = true;
+    for (int j = 0; j < initialCoords.getCols(); j++)
+    {
+        if (!almostEqual(m_A(0, j), -initialCoords(1, j)) || !almostEqual(m_A(1, j), initialCoords(0, j)))
+        {
+            cout << "Failed mapping: ";
+            cout << "(" << initialCoords(0, j) << ", " << initialCoords(1, j) << ") ==> (" << m_A(0, j) << ", " << m_A(1, j) << ")" << endl;
+            rotationPassed = false;
+        }
+    }
+    if (rotationPassed)
+    {
+        cout << "Passed.  +1" << endl;
+        score++;
+    }
+    else
+    {
+        cout << "Failed." << endl;
+    }
+
+    cout << "Applying a scale of 0.5..." << endl;
+    initialCoords = m_A;
+    scale(0.5);
+    bool scalePassed = true;
+    for (int j = 0; j < initialCoords.getCols(); j++)
+    {
+        if (!almostEqual(m_A(0, j), 0.5 * initialCoords(0, j)) || !almostEqual(m_A(1, j), 0.5 * initialCoords(1, j)))
+        {
+            cout << "Failed mapping: ";
+            cout << "(" << initialCoords(0, j) << ", " << initialCoords(1, j) << ") ==> (" << m_A(0, j) << ", " << m_A(1, j) << ")" << endl;
+            scalePassed = false;
+        }
+    }
+    if (scalePassed)
+    {
+        cout << "Passed.  +1" << endl;
+        score++;
+    }
+    else
+    {
+        cout << "Failed." << endl;
+    }
+
+    cout << "Applying a translation of (10, 5)..." << endl;
+    initialCoords = m_A;
+    translate(10, 5);
+    bool translatePassed = true;
+    for (int j = 0; j < initialCoords.getCols(); j++)
+    {
+        if (!almostEqual(m_A(0, j), 10 + initialCoords(0, j)) || !almostEqual(m_A(1, j), 5 + initialCoords(1, j)))
+        {
+            cout << "Failed mapping: ";
+            cout << "(" << initialCoords(0, j) << ", " << initialCoords(1, j) << ") ==> (" << m_A(0, j) << ", " << m_A(1, j) << ")" << endl;
+            translatePassed = false;
+        }
+    }
+    if (translatePassed)
+    {
+        cout << "Passed.  +1" << endl;
+        score++;
+    }
+    else
+    {
+        cout << "Failed." << endl;
+    }
+
+    cout << "Score: " << score << " / 7" << endl;
 }
 
 ///rotate Particle by theta radians counter-clockwise
